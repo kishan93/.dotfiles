2632871e14ccfb0064bb5e0322afeb5991c15768
Kishan Nath <kishansnath93@gmail.com>
2023-12-27 12:29:42 +0530

    refactored stow dirs

--- nvim/.config/nvim/lua/user/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-
-vim.g.copilot_filetypes = {
-  xml = false,
-  markdown = false
-}
-
-vim.g.copilot_no_tab_map = true
-vim.keymap.set("i", "<C-t>", ":copilot#Accept,('\\<CR>')<CR>", { silent = true })
-
