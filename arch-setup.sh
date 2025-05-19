190795419f78cc5282d2d1509b4924b40541c530
kishan nath <kishansnath93@gmail.com>
2025-05-19 10:55:00 +0530

    added wezterm

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # nvidia drivers
-sudo pacman -S nvidia nvidia-utils nvidia-settings egl-wayland
+sudo pacman -S --noconfirm --needed nvidia nvidia-utils nvidia-settings egl-wayland
+
 sudo echo "options nvidia_drm modeset=1" > /etc/modprobe.d/nvidia.conf
 
-sudo pacman -S --noconfirm zsh
+sudo pacman -S --noconfirm --needed zsh
 chsh -s $(which zsh)
 
-sudo pacman -S --noconfirm tmux
+sudo pacman -S --noconfirm --needed wezterm
 
-sudo pacman -S --noconfirm neovim lua luarocks
+sudo pacman -S --noconfirm --needed neovim lua luarocks
 sudo luarocks install luacheck
 
 sudo pacman -S --noconfirm --needed tldr
